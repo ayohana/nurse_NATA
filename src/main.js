e83d4a38a091bccb83e506c62ddbdc495ca02a9e
Adela Darmansyah and Neha Doodi <neha@>
2020-02-17 11:38:59 -0800

    Enter user input into UI, create vacationRequest object in form

--- src/main.js
+++ src/main.js
@@ -5,6 +5,18 @@ import $ from 'jquery';
 import { VacationRequest } from '../src/vacation-request';
 
 $(document).ready(function(){
-
+  $("form#requestForm").submit(function(event){
+    event.preventDefault();
+    let firstName = $("#firstName").val();
+    let lastName = $("#lastName").val();
+    let submissionDate = $("#submissionDate").val();
+    let vacationStartDate = $("#vacationStartDate").val();
+    let workReturnDate = $("#workReturnDate").val();
+    let workDaysRequestedOff = $("#workDaysRequestedOff").val();
+    let vacationHoursAvailable = $("#vacationHoursAvailable").val();
+    let comments = $("#comments").val();
+    let vacationRequest = new VacationRequest(firstName, lastName, submissionDate, vacationStartDate, workReturnDate, workDaysRequestedOff, vacationHoursAvailable, comments);
+    console.log(vacationRequest);
+  });
 
 });
--- src/vacation-request.js
+++ src/vacation-request.js
@@ -1,26 +1,17 @@
 export class VacationRequest {
-  constructor () {
-    this.name;
-    this.submissionDate;
-    this.vacationStartDate;
-    this.workReturnDate;
-    this.workDaysRequestedOff;
-    this.vacationHoursAvailable;
-    this.comments;
-    this.vacationApproval = false;
-    this.approvedVacationStartDate;
-    this.approvedVacationEndDate;
-    this.approvedWorkReturnDate;
-  }
-
-  submitRequest(name, submissionDate, vacationStartDate, workReturnDate, workDaysRequestedOff, vacationHoursAvailable, comments){
-    this.name = name;
+  constructor(firstName, lastName, submissionDate, vacationStartDate, workReturnDate, workDaysRequestedOff, vacationHoursAvailable, comments) {
+    this.firstName = firstName;
+    this.lastName = lastName;
     this.submissionDate = submissionDate;
     this.vacationStartDate = vacationStartDate;
     this.workReturnDate = workReturnDate;
     this.workDaysRequestedOff = workDaysRequestedOff;
     this.vacationHoursAvailable = vacationHoursAvailable;
     this.comments = comments;
+    this.vacationApproval = false;
+    this.approvedVacationStartDate;
+    this.approvedVacationEndDate;
+    this.approvedWorkReturnDate;
   }
 
 }
