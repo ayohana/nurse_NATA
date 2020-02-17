f1d35b8fbd77b9b6acef870c87d1ef1707327568
Adela Darmansyah and Neha Doodi <adela@>
2020-02-17 11:13:50 -0800

    Add form to index.html

--- src/main.js
+++ src/main.js
@@ -2,3 +2,9 @@ import 'bootstrap' ;
 import 'bootstrap/dist/css/bootstrap.min.css' ; 
 import './styles.css' ;
 import $ from 'jquery';
+import { VacationRequest } from '../src/vacation-request';
+
+$(document).ready(function(){
+
+
+});
--- src/vacation-request.js
+++ src/vacation-request.js
@@ -1,5 +1,19 @@
 export class VacationRequest {
   constructor () {
+    this.name;
+    this.submissionDate;
+    this.vacationStartDate;
+    this.workReturnDate;
+    this.workDaysRequestedOff;
+    this.vacationHoursAvailable;
+    this.comments;
+    this.vacationApproval = false;
+    this.approvedVacationStartDate;
+    this.approvedVacationEndDate;
+    this.approvedWorkReturnDate;
+  }
+
+  submitRequest(name, submissionDate, vacationStartDate, workReturnDate, workDaysRequestedOff, vacationHoursAvailable, comments){
     this.name = name;
     this.submissionDate = submissionDate;
     this.vacationStartDate = vacationStartDate;
@@ -7,9 +21,6 @@ export class VacationRequest {
     this.workDaysRequestedOff = workDaysRequestedOff;
     this.vacationHoursAvailable = vacationHoursAvailable;
     this.comments = comments;
-    this.vacationApproval = false;
-    this.approvedVacationStartDate;
-    this.approvedVacationEndDate;
-    this.approvedWorkReturnDate;
   }
+
 }
