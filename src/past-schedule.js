e3325557a66d983f001a77223cb74d5ae497a4d7
Tiffany Siu <tsiu88@gmail.com>
2020-02-19 10:56:51 -0800

    Fix spacing warnings in past-schedule

--- src/past-schedule.js
+++ src/past-schedule.js
@@ -1,17 +1,17 @@
 export class PastSchedule {
-	constructor (){
-		this.daysWorked = [];
-		this.priorVacationDates = [];
-	}
-	
-	savePastSchedule(datesWorked){
-		datesWorked.sort(function(a, b){return a-b});
-		this.daysWorked.push(datesWorked);
-	}
+  constructor (){
+    this.daysWorked = [];
+    this.priorVacationDates = [];
+  }
+  
+  savePastSchedule(datesWorked){
+    datesWorked.sort(function(a, b){return a-b;});
+    this.daysWorked.push(datesWorked);
+  }
 
-	savePriorVacationDates(vacationDates){
-		vacationDates.sort(function(a, b){return a-b});
-		this.priorVacationDates.push(vacationDates);
-	}
+  savePriorVacationDates(vacationDates){
+    vacationDates.sort(function(a, b){return a-b;});
+    this.priorVacationDates.push(vacationDates);
+  }
 
 }
