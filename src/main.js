e5bd7aeabc75e3f46b3dc2c3914b40f3ad4e29b4
Student <example@example.com>
2020-02-20 14:36:07 -0800

    Attempt to fix showPastHolidaysWorked

--- src/main.js
+++ src/main.js
@@ -112,13 +112,16 @@ function showPriorVacations(request, nurse) {
 // Check if Nurse worked on the holidays in the past 2 years
 function showPastHolidaysWorked(nurse, unit) {
   let workedPastHolidays;
+  // let pastHolidaysWorked = [];
   for (let m = 0; m < nurse.vacationRequests.length; m++) {
     for (let i = 0; i < nurse.pastSchedule2019.priorVacationDates.length; i++) {
       workedPastHolidays = nurse.compareWithPastHolidaysWorked(nurse.vacationRequests[m].vacationReqDateRange, nurse.pastSchedule2019.daysWorked, unit.holidays2019);
       if (workedPastHolidays.length === 0) {
-        console.log(`${nurse.firstName} ${nurse.lastName} did not work any holidays in 2019.`);
+        // pastHolidaysWorked.push(`${nurse.firstName} ${nurse.lastName} did not work any holidays in 2019.`)
+        console.log(`${nurse.firstName} ${nurse.lastName} did not work any holidays in 2019.`)
       } else {
-        console.log(`${nurse.firstName} ${nurse.lastName} worked on these holidays in 2019: ${workedPastHolidays}. They may have their vacation request approved this year.`);
+        // pastHolidaysWorked.push(`${nurse.firstName} ${nurse.lastName} worked on these holidays in 2019: ${workedPastHolidays}. They may have their vacation request approved this year.`)
+        console.log(`${nurse.firstName} ${nurse.lastName} worked on these holidays in 2019: ${workedPastHolidays}. They may have their vacation request approved this year.`)
       }
     }
   }
@@ -126,17 +129,23 @@ function showPastHolidaysWorked(nurse, unit) {
     for (let i = 0; i < nurse.pastSchedule2018.priorVacationDates.length; i++) {
       workedPastHolidays = nurse.compareWithPastHolidaysWorked(nurse.vacationRequests[m].vacationReqDateRange, nurse.pastSchedule2018.daysWorked, unit.holidays2018);
       if (workedPastHolidays.length === 0) {
-        console.log(`${nurse.firstName} ${nurse.lastName} did not work any holidays in 2018.`);
+        // pastHolidaysWorked.push(`${nurse.firstName} ${nurse.lastName} did not work any holidays in 2018.`);
+        console.log(`${nurse.firstName} ${nurse.lastName} did not work any holidays in 2018.`)
       } else {
-        console.log(`${nurse.firstName} ${nurse.lastName} worked on these holidays in 2018: ${workedPastHolidays}.`);
+        // pastHolidaysWorked.push(`${nurse.firstName} ${nurse.lastName} worked on these holidays in 2018: ${workedPastHolidays}.`);
+        console.log(`${nurse.firstName} ${nurse.lastName} worked on these holidays in 2018: ${workedPastHolidays}.`)
       }
     }
   }
+  //console.log(pastHolidaysWorked);
+  //return pastHolidaysWorked;
 }
 
 // Show message for if there are overlapping vacation request dates within group of a nurse type
 function showStaffOverlapVacReqs(unit, sortedStaff){
   let overlapVacReqs = unit.compareVacationRequests(sortedStaff);
+  console.log(unit);
+  console.log(sortedStaff);
   if ($.isEmptyObject(overlapVacReqs)) {
     console.log(`${sortedStaff[0].role}s in this Unit do not have any overlapping vacation requests.`);
   } else {
@@ -178,21 +187,21 @@ $(document).ready(function(){
   let workDates = 1;
 
   // HARD-CODED TEST NURSE INFO
-  let nurseG = new Nurse("mary", "smith", new Date("2013/06/09"), 1356, [1/3], "CN", 0.6);
-  let nurseH = new Nurse("patricia", "johnson", new Date("2011/08/15"), 2356, [1/2], "CN", 0.9);
-  let nurseI = new Nurse("linda", "williams", new Date("2003/02/14"), 5413, [], "CN", 0.3);
+  let nurseG = new Nurse("mary", "smith", new Date("2013/06/09"), 1356, "CN", 0.6);
+  let nurseH = new Nurse("patricia", "johnson", new Date("2011/08/15"), 2356, "CN", 0.9);
+  let nurseI = new Nurse("linda", "williams", new Date("2003/02/14"), 5413, "CN", 0.3);
  
-  let nurseJ = new Nurse("barbara", "jones", new Date("2007/04/17"), 7439, [1/3], "RN", 0.6);
-  let nurseK = new Nurse("elizabeth", "brown", new Date("2002/08/08"), 4523, [1/2], "RN", 0.9);
-  let nurseL = new Nurse("jennifer", "davis", new Date("1998/07/11"), 1743, [], "RN", 0.3);
-  let nurseM = new Nurse("maria", "miller", new Date("2014/02/19"), 8482, [], "RN", 0.9);
+  let nurseJ = new Nurse("barbara", "jones", new Date("2007/04/17"), 7439, "RN", 0.6);
+  let nurseK = new Nurse("elizabeth", "brown", new Date("2002/08/08"), 4523, "RN", 0.9);
+  let nurseL = new Nurse("jennifer", "davis", new Date("1998/07/11"), 1743, "RN", 0.3);
+  let nurseM = new Nurse("maria", "miller", new Date("2014/02/19"), 8482, "RN", 0.9);
 
-  let nurseA = new Nurse("susan", "thomas", new Date("2000/01/02"), 1234, [2/3, 4/5], "NAC", 0.6);
-  let nurseB = new Nurse("lisa", "lee", new Date("2011/02/08"), 3456, [1/3], "NAC", 0.6);
-  let nurseC = new Nurse("nancy", "wright", new Date("2005/12/21"), 2345, [1/2], "NAC", 0.9);
-  let nurseD = new Nurse("betty", "young", new Date("2008/03/14"), 543, [], "NAC", 0.3);
-  let nurseE = new Nurse("carol", "turner", new Date("2018/01/24"), 5432, [], "NAC", 0.9);
-  let nurseF = new Nurse("laura", "adams", new Date("2002/05/03"), 1543, [], "NAC", 0.9);
+  let nurseA = new Nurse("susan", "thomas", new Date("2000/01/02"), 1234, "NAC", 0.6);
+  let nurseB = new Nurse("lisa", "lee", new Date("2011/02/08"), 3456, "NAC", 0.6);
+  let nurseC = new Nurse("nancy", "wright", new Date("2005/12/21"), 2345, "NAC", 0.9);
+  let nurseD = new Nurse("betty", "young", new Date("2008/03/14"), 543, "NAC", 0.3);
+  let nurseE = new Nurse("carol", "turner", new Date("2018/01/24"), 5432, "NAC", 0.9);
+  let nurseF = new Nurse("laura", "adams", new Date("2002/05/03"), 1543, "NAC", 0.9);
 
   // Add nurses to units, sort, and output to MVP Output Priority list
   addNurseToUnit(nurseA, unit);
@@ -227,6 +236,7 @@ $(document).ready(function(){
 
   //showPriorVacations(nurseA);
   showPastHolidaysWorked(nurseA, unit);
+  console.log("function we alloking for"+ showPastHolidaysWorked(nurseA, unit));
   showStaffOverlapVacReqs(unit, unit.sortedChargeNurses);
   showStaffOverlapVacReqs(unit, unit.sortedRegisteredNurses);
   showStaffOverlapVacReqs(unit, unit.sortedNursingAssistants);
@@ -253,9 +263,11 @@ $(document).ready(function(){
     let comments = $("#comments").val();
 
     let vacationRequest = new VacationRequest(firstName, lastName, submissionDate, vacationStartDate, workReturnDate, workDaysRequestedOff, vacationHoursAvailable, comments);
-
+ 
     vacationRequest.checkVacationHoursAvailable();
     let currentNurse = unit.searchNurse(firstName, lastName);
+    let pastHolidaysWorked = showPastHolidaysWorked(currentNurse, unit);
+    console.log(pastHolidaysWorked);
     if (vacationRequest.checkVacationSubmissionDate(unit.requestDueDate)){
 
       if(vacationRequest.adequateVacationHours === true){
@@ -281,7 +293,7 @@ $(document).ready(function(){
             $("#vacationMessage").text("Vacation request succesfully submitted!");
           }
         }
-
+        // $("#workedHolidayMessage").text()
       } else {
         $("#vacationMessage").text(`Insufficient vacation hours available!  Available Hours: ${vacationHoursAvailable} Requested Hours: ${vacationRequest.hoursRequestedOff()}`);
       }
